1b974305fdecfe3e261674e291b9bf7b18a9ec75
Batyr Sardarbekov <aaabat71@gmail.com>
2020-07-09 21:18:55 +0600

    sandbox

--- marketplace/sandbox/views.py
+++ marketplace/sandbox/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def sandboxView(request):
     context = {
-        'name': 'Dias',
+        'name': 's',
+        '123': 'asd'
     }
     return render(request, 'sandbox/index.html', context=context)
