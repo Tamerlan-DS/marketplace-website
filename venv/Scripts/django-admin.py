e335803b6394c3d4da6cb46024606356bb5f6bb9
Batyr Sardarbekov <aaabat71@gmail.com>
2020-07-10 23:11:20 +0600

    add venv

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\kekes\marketplace2\marketplace-for-companies-fullstack\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
