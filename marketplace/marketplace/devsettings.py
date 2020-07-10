e4073a9b0a1cc801cdb2060a3bd5aff428ab1a05
Dias <dias2001@inbox.ru>
2020-07-11 01:11:17 +0600

    HTML pages edit

--- marketplace/marketplace/devsettings.py
+++ marketplace/marketplace/devsettings.py
@@ -5,4 +5,5 @@ DEBUG = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+# STATIC_DIRS = [os.path.join(BASE_DIR, "static"), ]
+
