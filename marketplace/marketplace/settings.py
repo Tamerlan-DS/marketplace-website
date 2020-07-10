2062734d875e6fc25dc2c9e12bbe0d3125d0c90c
Batyr Sardarbekov <aaabat71@gmail.com>
2020-07-11 05:36:14 +0600

    setting for prod

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/home/t/topbuild/topbuild.beget.tech/public/static'
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
