815e52cf347fe3058fffa3566f309a8d127c6b82
Batyr Sardarbekov <aaabat71@gmail.com>
2020-07-10 20:44:00 +0600

    setting for prod

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -11,134 +11,112 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from configurations import Configuration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-class Base(Configuration):
-    # Quick-start development settings - unsuitable for production
-    # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'k9(=yffp1@@-p6d9nxl)e8orn6ip9+o%gth4q&7=x9ypko$6vt'
 
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = 'k9(=yffp1@@-p6d9nxl)e8orn6ip9+o%gth4q&7=x9ypko$6vt'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
+ALLOWED_HOSTS = ['topbuild.beget.tech', '127.0.0.1', 'localhost']
 
-    ALLOWED_HOSTS = []
+# Application definition
 
-    # Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'company',
+    'admin_panel',
+    'sandbox',
+]
 
-    INSTALLED_APPS = [
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',
-        'company',
-        'admin_panel',
-        'sandbox',
-    ]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
-    MIDDLEWARE = [
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ]
+ROOT_URLCONF = 'marketplace.urls'
 
-    ROOT_URLCONF = 'marketplace.urls'
-
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [],
-            'APP_DIRS': True,
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
-                ],
-            },
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
         },
-    ]
+    },
+]
 
-    WSGI_APPLICATION = 'marketplace.wsgi.application'
+WSGI_APPLICATION = 'marketplace.wsgi.application'
 
-    # Database
-    # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
-    # Password validation
-    # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-    AUTH_PASSWORD_VALIDATORS = [
-        {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-        },
-    ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
-    # Internationalization
-    # https://docs.djangoproject.com/en/3.0/topics/i18n/
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-    LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'
 
-    TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
-    USE_I18N = True
+USE_I18N = True
 
-    USE_L10N = True
+USE_L10N = True
 
-    USE_TZ = True
+USE_TZ = True
 
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
-    # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-
-class Dev(Base):
-    DEBUG = True
-    ALLOWED_HOSTS = ['*', ]
-    TIME_ZONE = 'Asia/Almaty'
-
-
-class Prod(Base):
-    DEBUG = False
-    STATIC_ROOT = "/static"
-    MEDIA_ROOT = "/media"
-    PRIVATE_ROOT = "/private"
-
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
-    # SITE_URL = ""
-    ALLOWED_HOSTS = ['topbuild.beget.tech', '127.0.0.1', 'localhost']
-
-    TIME_ZONE = 'UTC'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
