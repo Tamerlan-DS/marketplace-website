c0718cc61eab740e87a426bf06e1d6bf4ccecabf
Dias <dias2001@inbox.ru>
2020-07-10 23:17:48 +0600

    admin panel pages added.

--- marketplace/admin_panel/urls.py
+++ marketplace/admin_panel/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('companies', companyView, name='companies'),
+    path('company-edit', companyEditView, name='company-edit'),
+    path('company-category', companyCategoryView, name='company-category'),
+    path('company-category-edit', companyCategoryEditView, name='company-category-edit'),
 ]
