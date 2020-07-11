9e0b822d07e2d1ea1c9f3d020514840bbd3f674c
Batyr Sardarbekov <aaabat71@gmail.com>
2020-07-11 21:12:00 +0600

    deleted collected static

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\kekes\marketplace2\marketplace-for-companies-fullstack\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
