0d1766ebff0727a9f4f830b701f112e6ca475666
Batyr Sardarbekov <aaabat71@gmail.com>
2020-07-11 21:00:28 +0600

    setting for prod

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT =  '/home/t/topbuild/topbuild.beget.tech/public_html/static'
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = '/home/t/topbuild/topbuild.beget.tech/public_html/static'
