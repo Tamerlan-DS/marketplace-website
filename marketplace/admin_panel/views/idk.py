c9d7ccff55fe71df8da278f16977b5abc80c4c88
batrr <aaabat71@gmail.com>
2020-09-08 11:31:54 +0600

    add history

--- marketplace/admin_panel/views/idk.py
+++ marketplace/admin_panel/views/idk.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from admin_panel.decorators import *
+from company_panel.models import Invoice
+
 
 @login_required
+@user_is_moder
 def adminPanelView(request):
+    history = Invoice.objects.all().order_by('-date')
     context = {
+        'history': history,
     }
     return render(request, 'admin_panel/index.html', context=context)
