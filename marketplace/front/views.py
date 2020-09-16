e88df56721daec4c85f3a6ae127381001ad92ca7
Dias <dias2001@inbox.ru>
2020-09-16 12:02:19 +0600

    Front added

--- marketplace/front/views.py
+++ marketplace/front/views.py
@@ -10,3 +10,13 @@ def testView(request):
         'username': username,
     }
     return render(request, 'front/test.html', context=context)
+
+def FrontPageView(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        username = 'anon'
+    context = {
+        'username': username,
+    }
+    return render(request, 'front/index.html',context=context)
