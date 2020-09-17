6283f5293a1c47c33fb99e9475485ea0dcf9a842
Dias <dias2001@inbox.ru>
2020-09-17 15:18:05 +0600

    Front natyanut

--- marketplace/front/urls.py
+++ marketplace/front/urls.py
@@ -3,4 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', FrontPageView, name='FrontPage'),
+    path('catalog', catalogPageView, name='Catalog'),
+    path('catalog-item', catalogItemPageView, name='Catalog-item'),
+
+    path('contacts', contactPageView, name='contact'),
+
+    path('default', defaultPageView, name='default'),
+    path('error', errorPageView, name='error'),
 ]
--- marketplace/front/views.py
+++ marketplace/front/views.py
@@ -19,4 +19,59 @@ def FrontPageView(request):
     context = {
         'username': username,
     }
+
     return render(request, 'front/index.html',context=context)
+
+
+def catalogPageView(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        username = 'anon'
+    context = {
+        'username': username,
+    }
+    return render(request, 'front/catalog.html',context=context)
+
+
+def catalogItemPageView(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        username = 'anon'
+    context = {
+        'username': username,
+    }
+    return render(request, 'front/catalog-item.html',context=context)
+
+
+def contactPageView(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        username = 'anon'
+    context = {
+        'username': username,
+    }
+    return render(request, 'front/contact.html',context=context)
+
+
+def defaultPageView(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        username = 'anon'
+    context = {
+        'username': username,
+    }
+    return render(request, 'front/default.html',context=context)
+
+def errorPageView(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        username = 'anon'
+    context = {
+        'username': username,
+    }
+    return render(request, 'front/error.html',context=context)
