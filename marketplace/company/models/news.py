403c81b16a7fadd822d2fb0ed8db5c1c2ec54db9
batrr <aaabat71@gmail.com>
2020-09-24 22:37:03 +0600

    date add

--- marketplace/company/models/news.py
+++ marketplace/company/models/news.py
@@ -5,3 +5,7 @@ class News(models.Model):
     title = models.CharField(max_length=50, default="")
     description = models.TextField(default="")
     text = models.TextField(default="")
+    date = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
